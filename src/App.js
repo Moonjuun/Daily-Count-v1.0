7d64ef9328a5dbaaaa760e78af8a1354eacfa705
Jun <cmoonjun11@gmail.com>
2023-04-08 15:40:08 +0900

    css 수정

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import CommonCal from "./pages/CommonCal";
+import WordCal from "./pages/WordCal";
+import DateCal from "./pages/DateCal";
+import BmiCal from "./pages/BmiCal";
 
 function App() {
   const [theme, setTheme] = useState("light");
@@ -16,18 +19,25 @@ function App() {
 
   return (
     <BrowserRouter>
-      <article data-theme={theme}>
-        <div className="container">
-          <Header theme={theme} toggleTheme={toggleTheme} />
-          <Menu />
-          <Routes>
-            <Route path="/" element={<CommonCal />}></Route>
-          </Routes>
-        </div>
-        <footer style={{ textAlign: "center" }}>
-          <p> © 2023 cmoonjun11@gmail.com</p>
-        </footer>
-      </article>
+      <div className="App">
+        <article data-theme={theme}>
+          <div className="container">
+            <Header theme={theme} toggleTheme={toggleTheme} />
+            <Menu />
+            <div className="jb-division-line"></div>
+
+            <Routes>
+              <Route path="/" element={<CommonCal />}></Route>
+              <Route path="/WordCal" element={<WordCal />}></Route>
+              <Route path="/DateCal" element={<DateCal />}></Route>
+              <Route path="/BmiCal" element={<BmiCal />}></Route>
+            </Routes>
+          </div>
+          <footer style={{ textAlign: "center" }}>
+            <p> © 2023 cmoonjun11@gmail.com</p>
+          </footer>
+        </article>
+      </div>
     </BrowserRouter>
   );
 }

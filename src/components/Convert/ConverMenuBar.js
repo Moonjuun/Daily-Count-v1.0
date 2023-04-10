bcb3fdcb9270bc963824d123e79fd6b05d9497ad
Jun <cmoonjun11@gmail.com>
2023-04-10 15:35:21 +0900

    단위환산 nav바 모바일일 때 2줄로 보이게 설정

--- src/components/Convert/ConverMenuBar.js
+++ src/components/Convert/ConverMenuBar.js
@@ -9,60 +9,141 @@ const ConvertMenuBar = () => {
     navigate(path);
   };
 
+  const isMobile = window.innerWidth < 768; // 가로 크기가 768px 미만이면 모바일로 간주합니다.
+
   return (
     <div>
-      <nav aria-label="breadcrumb">
-        <ul>
-          <li>
-            <a href="/" onClick={(event) => handleClick(event, "/LengthCal")}>
-              길이
-            </a>
-          </li>
-          <li>
-            <a href="/" onClick={(event) => handleClick(event, "/AreaCal")}>
-              넓이
-            </a>
-          </li>
-          <li>
-            <a href="/" onClick={(event) => handleClick(event, "/WeightCal")}>
-              무게
-            </a>
-          </li>
-          <li>
-            <a href="/" onClick={(event) => handleClick(event, "/VolumeCal")}>
-              부피
-            </a>
-          </li>
-          <li>
-            <a
-              href="/"
-              onClick={(event) => handleClick(event, "/TemperatureCal")}
-            >
-              온도
-            </a>
-          </li>
-          <li>
-            <a href="/" onClick={(event) => handleClick(event, "/PressCal")}>
-              압력
-            </a>
-          </li>
-          <li>
-            <a href="/" onClick={(event) => handleClick(event, "/VelocityCal")}>
-              속도
-            </a>
-          </li>
-          <li>
-            <a href="/" onClick={(event) => handleClick(event, "/FuelCal")}>
-              연비
-            </a>
-          </li>
-          <li>
-            <a href="/" onClick={(event) => handleClick(event, "/DataCal")}>
-              데이터양
-            </a>
-          </li>
-        </ul>
-      </nav>
+      {isMobile ? (
+        <>
+          <nav aria-label="breadcrumb">
+            <ul>
+              <li>
+                <a
+                  href="/"
+                  onClick={(event) => handleClick(event, "/LengthCal")}
+                >
+                  길이
+                </a>
+              </li>
+              <li>
+                <a href="/" onClick={(event) => handleClick(event, "/AreaCal")}>
+                  넓이
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  onClick={(event) => handleClick(event, "/WeightCal")}
+                >
+                  무게
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  onClick={(event) => handleClick(event, "/VolumeCal")}
+                >
+                  부피
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <nav aria-label="breadcrumb">
+            <ul>
+              <li>
+                <a
+                  href="/"
+                  onClick={(event) => handleClick(event, "/TemperatureCal")}
+                >
+                  온도
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  onClick={(event) => handleClick(event, "/PressCal")}
+                >
+                  압력
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/"
+                  onClick={(event) => handleClick(event, "/VelocityCal")}
+                >
+                  속도
+                </a>
+              </li>
+              <li>
+                <a href="/" onClick={(event) => handleClick(event, "/FuelCal")}>
+                  연비
+                </a>
+              </li>
+              <li>
+                <a href="/" onClick={(event) => handleClick(event, "/DataCal")}>
+                  데이터
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </>
+      ) : (
+        <nav aria-label="breadcrumb">
+          <ul style={{ fontSize: "18px" }}>
+            <li>
+              <a href="/" onClick={(event) => handleClick(event, "/LengthCal")}>
+                길이
+              </a>
+            </li>
+            <li>
+              <a href="/" onClick={(event) => handleClick(event, "/AreaCal")}>
+                넓이
+              </a>
+            </li>
+            <li>
+              <a href="/" onClick={(event) => handleClick(event, "/WeightCal")}>
+                무게
+              </a>
+            </li>
+            <li>
+              <a href="/" onClick={(event) => handleClick(event, "/VolumeCal")}>
+                부피
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                onClick={(event) => handleClick(event, "/TemperatureCal")}
+              >
+                온도
+              </a>
+            </li>
+            <li>
+              <a href="/" onClick={(event) => handleClick(event, "/PressCal")}>
+                압력
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                onClick={(event) => handleClick(event, "/VelocityCal")}
+              >
+                속도
+              </a>
+            </li>
+            <li>
+              <a href="/" onClick={(event) => handleClick(event, "/FuelCal")}>
+                연비
+              </a>
+            </li>
+            <li>
+              <a href="/" onClick={(event) => handleClick(event, "/DataCal")}>
+                데이터
+              </a>
+            </li>
+          </ul>
+        </nav>
+      )}
     </div>
   );
 };

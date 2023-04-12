8a6636611a58c2069df23d794e418152c8d7a85c
Jun <cmoonjun11@gmail.com>
2023-04-12 22:00:02 +0900

    일반 계산기 소수점 이하일 때 천자리 콤마 표시 x

--- src/components/CommonCal/ResultComponent.js
+++ src/components/CommonCal/ResultComponent.js
@@ -1,9 +1,19 @@
 import React from "react";
 
 const ResultComponent = ({ result }) => {
-  const formattedResult = result
-    .toString()
-    .replace(/\B(?=(\d{3})+(?!\d))/g, ","); // 천 단위 쉼표 추가
+  let formattedResult = result.toString();
+
+  const decimalIndex = formattedResult.indexOf(".");
+  if (decimalIndex !== -1) {
+    // 소수점 이하 숫자가 있는 경우
+    const integerPart = formattedResult.slice(0, decimalIndex);
+    const decimalPart = formattedResult.slice(decimalIndex + 1);
+    formattedResult =
+      integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "." + decimalPart;
+  } else {
+    // 소수점 이하 숫자가 없는 경우
+    formattedResult = formattedResult.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 
   return (
     <div className="CommonCal">

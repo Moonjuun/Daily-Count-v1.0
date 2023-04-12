b7bd80f54657b1413fa5ee013176da6b30d0c64d
Jun <cmoonjun11@gmail.com>
2023-04-12 13:38:01 +0900

    sdk 버전으로 인한 카카오톡 공유 에러 픽스

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -64,7 +64,9 @@ const Footer = () => {
   const currentUrl = window.location.href;
 
   // kakao SDK import하기
-  const status = useScript("https://developers.kakao.com/sdk/js/kakao.js");
+  const status = useScript(
+    "https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js"
+  );
 
   // kakao sdk 초기화하기
   // status가 변경될 때마다 실행되며, status가 ready일 때 초기화를 시도합니다.
@@ -79,7 +81,7 @@ const Footer = () => {
   }, [status]);
 
   const handleKakaoButton = () => {
-    window.Kakao.Link.sendScrap({
+    window.Kakao.Share.sendScrap({
       requestUrl: currentUrl,
     });
   };

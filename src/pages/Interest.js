60b64ddf711570190a82d1698d6c4222ffd7ab7b
Jun <cmoonjun11@gmail.com>
2023-04-15 18:48:08 +0900

    이자 계산기 수정

--- src/pages/Interest.js
+++ src/pages/Interest.js
@@ -1,104 +1,167 @@
 import React, { useState } from "react";
+import { commaFormat, uncommaFormat } from "../utils/util";
 
 function Interest() {
-  const [monthlyAmount, setMonthlyAmount] = useState("");
-  const [savingPeriod, setSavingPeriod] = useState("");
-  const [interestRate, setInterestRate] = useState("");
-  const [taxOption, setTaxOption] = useState("general");
-  const [interestAmount, setInterestAmount] = useState("");
+  const [inputValues, setInputValues] = useState({
+    monthlyDeposit: "",
+    termLength: "",
+    termUnit: "months",
+    interestRate: "",
+    isCompoundInterest: "simple",
+  });
 
-  const calculateInterest = () => {
-    const monthlyInterestRate = interestRate / 12 / 100; // 월 이자율 계산
-    const totalSavingAmount = monthlyAmount * savingPeriod; // 총 납입금액 계산
-    let taxableInterestAmount = 0; // 과세 대상 이자금액
-    let taxAmount = 0; // 세금액
+  const [result, setResult] = useState(null);
+  console.log("aaa", { ...inputValues });
 
-    // 이자과세에 따라 과세 대상 이자금액과 세금액을 계산
-    if (taxOption === "general") {
-      // 일반과세
-      taxableInterestAmount = interestAmount * 0.6;
-      taxAmount = taxableInterestAmount * 0.15;
-    } else if (taxOption === "tax-preferred") {
-      // 세금우대
-      taxableInterestAmount = interestAmount * 0.7;
-      taxAmount = taxableInterestAmount * 0.045;
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputValues({ ...inputValues, [name]: parseFloat(value) });
+  };
+
+  const handleRadioChange = (e) => {
+    const { name, value } = e.target;
+    setInputValues({ ...inputValues, [name]: value });
+  };
+
+  const calculateInterest = (inputValues) => {
+    const {
+      monthlyDeposit,
+      termLength,
+      termUnit,
+      interestRate,
+      isCompoundInterest,
+    } = inputValues;
+
+    console.log("bbb", { ...inputValues });
+
+    //년
+    if (termUnit === "years") {
+      console.log("years");
+      const makeMonth = termLength * 12;
+      // 단리
+      if (isCompoundInterest === "simple") {
+        console.log("simple");
+        const totalAmount = monthlyDeposit * makeMonth * (interestRate / 100);
+        return totalAmount;
+      } else {
+        console.log("compound");
+        const monthlyInterestRate = interestRate / 12 / 100;
+        // 복리
+        const totalAmount =
+          monthlyDeposit *
+          ((Math.pow(1 + monthlyInterestRate, makeMonth) - 1) /
+            monthlyInterestRate);
+        return totalAmount;
+      }
+    } else {
+      console.log("months");
+      // 개월
+      if (isCompoundInterest === "simple") {
+        console.log("simple");
+        console.log("aa", interestRate);
+        const totalAmount = monthlyDeposit * termLength * (interestRate / 100);
+        return totalAmount;
+      } else {
+        console.log("compound");
+        const monthlyInterestRate = interestRate / 12 / 100;
+        // 복리
+        const totalAmount =
+          monthlyDeposit *
+          ((Math.pow(1 + monthlyInterestRate, termLength) - 1) /
+            monthlyInterestRate);
+        return totalAmount;
+      }
     }
+  };
 
-    // 이자 계산 결과를 interestAmount state에 저장
-    const interest = totalSavingAmount * monthlyInterestRate * savingPeriod;
-    setInterestAmount(interest);
-
-    // 세금 공제 후 이자 계산 결과를 보여줄 엘리먼트를 추가해주세요.
-    const finalInterest = interest - taxAmount;
-    console.log(
-      `이자: ${interest}원, 세금: ${taxAmount}원, 공제 후 이자: ${finalInterest}원`
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // 계산 로직을 작성하여 결과값을 계산합니다.
+    const calculatedResult = calculateInterest(inputValues);
+    setResult(calculatedResult);
   };
 
   return (
-    <div>
-      <h1>적금 계산기</h1>
+    <form onSubmit={handleSubmit}>
       <label>
-        월적립액:
+        월 적금액
         <input
-          type="number"
-          value={monthlyAmount}
-          onChange={(e) => setMonthlyAmount(e.target.value)}
+          type="text"
+          name="monthlyDeposit"
+          value={inputValues.monthlyDeposit}
+          onChange={handleInputChange}
         />
       </label>
       <br />
       <label>
-        적금기간:
+        적금 기간
         <input
-          type="number"
-          value={savingPeriod}
-          onChange={(e) => setSavingPeriod(e.target.value)}
+          type="text"
+          name="termLength"
+          value={inputValues.termLength}
+          onChange={handleInputChange}
+        />
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="termUnit"
+          value="months"
+          checked={inputValues.termUnit === "months"}
+          onChange={handleRadioChange}
         />
         개월
       </label>
+      <label>
+        <input
+          type="radio"
+          name="termUnit"
+          value="years"
+          checked={inputValues.termUnit === "years"}
+          onChange={handleRadioChange}
+        />
+        년
+      </label>
+
       <br />
       <label>
-        연이자율:
+        이자율
         <input
-          type="number"
-          step="0.01"
-          value={interestRate}
-          onChange={(e) => setInterestRate(e.target.value)}
+          type="text"
+          name="interestRate"
+          value={inputValues.interestRate}
+          onChange={handleInputChange}
         />
-        %
+      </label>
+
+      <label>
+        <input
+          type="radio"
+          name="isCompoundInterest"
+          value="simple"
+          checked={inputValues.isCompoundInterest === "simple"}
+          onChange={handleRadioChange}
+        />
+        단리
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="isCompoundInterest"
+          value="compound"
+          checked={inputValues.isCompoundInterest === "compound"}
+          onChange={handleRadioChange}
+        />
+        복리
       </label>
       <br />
-      <label>이자과세:</label>
-      <button
-        style={{ marginRight: "10px" }}
-        onClick={() => setTaxOption("general")}
-        className={taxOption === "general" ? "selected" : ""}
-      >
-        일반과세
-      </button>
-      <button
-        style={{ marginRight: "10px" }}
-        onClick={() => setTaxOption("tax-free")}
-        className={taxOption === "tax-free" ? "selected" : ""}
-      >
-        비과세
-      </button>
-      <button
-        onClick={() => setTaxOption("tax-preferred")}
-        className={taxOption === "tax-preferred" ? "selected" : ""}
-      >
-        세금우대
-      </button>
-      <br />
-      <button onClick={calculateInterest}>이자 계산하기</button>
-      <br />
-      <div>
-        이자:{" "}
-        {interestAmount !== ""
-          ? `${interestAmount.toLocaleString()}원`
-          : "계산 결과가 여기에 표시됩니다."}
-      </div>
-    </div>
+      <button type="submit">계산</button>
+      {result && (
+        <div>
+          Result: {result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </div>
+      )}
+    </form>
   );
 }
 
